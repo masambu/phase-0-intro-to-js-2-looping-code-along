aa6ca05c87b1148fdfed277adbf1f4c37d8e3491
Kelvin <kevohmasambu@gmail.com>
2023-12-17 22:23:36 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(names, giftType) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${giftType} gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+}
+
+
+
+
+function countDown(integer) {
+    for (let i = integer; i >= 0; i--) {
+        console.log(i);
+    }
+}
+countDown(10);
